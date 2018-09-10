b9887849f1a498ecef0a676c43c7bb7a2f729362
shivamKumarITT <shivam.kumar@intimetec.com>
2018-09-10 23:45:39 +0530

    added unit test for TDSConfiguration

---
+++ src/test/TDSConfigurationTEST.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.itt.tds.cfg.TDSConfiguration;
+
+class TDSConfigurationTEST {
+
+	@Test
+	void testGetInstance() {
+		TDSConfiguration tdsCFG1 = TDSConfiguration.getInstance();
+		TDSConfiguration tdsCFG2 = TDSConfiguration.getInstance();
+		TDSConfiguration tdsCFG3 = TDSConfiguration.getInstance();
+		TDSConfiguration tdsCFG4 = TDSConfiguration.getInstance();
+		
+		assertEquals(tdsCFG1.hashCode(), tdsCFG2.hashCode());
+		assertSame(tdsCFG3, tdsCFG4);
+	}
+
+	@Test
+	void testGetDBConnectionString() {
+		String dbConnectionString = "jdbc:mysql://localhost:3306/tds?user=root&password=password";
+		
+		TDSConfiguration tdsCFG = TDSConfiguration.getInstance();
+		String result = tdsCFG.getDBConnectionString();
+		
+		assertEquals(dbConnectionString, result);
+	}
+}
